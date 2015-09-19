93b54acff163f7f88d3f01bc1f9a88fd512b46dc
Guillaume MOREL <guillaume.morel@verylastroom.fr>
2015-09-19 18:19:33 +0200

    Add BiologicTestNotFoundException
    In ConsumableEngine/Application/Exception as used in Application validation
    Ie. Biologic Test not found in Database

---
+++ src/ConsumableEngine/Application/Exception/BiologicTestNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Api\ConsumableEngine\Application\Exception;
+
+/**
+ * @author Guillaume MOREL <guillaume.morel@verylastroom.com>
+ */
+class BiologicTestNotFoundException extends \Exception
+{
+
+}
